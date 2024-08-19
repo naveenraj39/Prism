599db4f740e5a5f525ee293c6ecce2d507722b64
naveenraj39 <123942201+naveenraj39@users.noreply.github.com>
2024-08-19 13:24:14 +0530

    test

--- src/test/java/qa/prism/OrgCreation.java
+++ src/test/java/qa/prism/OrgCreation.java
@@ -10,47 +10,44 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 @TestInstance(Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class OrgCreation_Test extends BaseClass{
+public class OrgCreation extends BaseClass{
 	SuperClass superOrg;
 	Integration_Setup IS;
+	
+	
 @BeforeAll
 public void invokeApplication() throws Exception {
 	superOrg = new SuperClass();
 	superOrg.setup_connection();
-}
-
-@Test
-@Order(1)
-public void signInToApplication() {
 	superOrg.login_prism_withValidUsername_validPassword();
 }
 
 @Test
-@Order(2)
+@Order(1)
 public void addOrgDetail() throws Exception {
 	superOrg.addOrgDetail();
 }
 
 @Test
-@Order(3)
+@Order(2)
 public void addOrgAdminDetails() {
 	superOrg.addAdminDetail();
 }
 
 @Test
-@Order(4)
+@Order(3)
 public void setAuthAndCreateNewOrg() {
 	superOrg.setupAuth();
 }
 
 @Test
-@Order(5)
+@Order(4)
 public void searchOrg() throws Exception {
 	superOrg.searchAndViewOrg();
 }
 
 @Test
-@Order(6)
+@Order(5)
 public void integration() throws Exception {
 	superOrg.smtpSetup();
 	superOrg.hl7Setup();
